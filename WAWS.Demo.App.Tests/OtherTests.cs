e2c4a56da0f5a6f4d16045895e541606420926a7
noopman <himself@noop.se>
2012-09-26 14:21:21 +0200

    added the test files

---
+++ WAWS.Demo.App.Tests/OtherTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WAWS.Demo.App.Tests
+{
+	[TestClass]
+	public class DemoTests
+	{
+		[TestMethod]
+		public void Some_Demo_Test()
+		{
+			Assert.Fail("This test is supposed to fail! It is a demo failure!");
+		}
+	}
+}
